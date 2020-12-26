82e2588fc88b32892a91c00484458d9b8605df7b
mertkarababa7 <62035132+mertkarababa7@users.noreply.github.com>
2020-12-26 06:13:07 +0300

    Add files via upload

---
+++ LoginCustomer.php
@@ -0,0 +1,48 @@
+<?php 
+session_start();
+
+?>
+
+<html>
+<head>
+<title>Customer Login</title>
+
+<link rel="stylesheet" href="Main.css">
+    
+</head>
+<div class="topnav">
+<a href="index.php" class="active">Login As Admin</a>
+ <a href="LoginCustomer.php" class="active">Login As Customer</a>
+  <a href="logout.php">Admin LogOut </a>
+  
+  
+ 
+</div>
+<body>
+  <header>    
+    <div id="login-box">
+       <div class="one-box">
+          <h2> Customer Login</h2>
+              <form action="Customer/LoginCustomerFunction.php" method="post">
+                             <div class="input-group">
+<?php  if (isset($_GET['error'])) {  ?>
+        <p class="error"><?php  echo   $_GET['error'] ; ?>  <?php } ?>
+        <h3>Username<br><br>
+        <span class="input-group-addon" id="basic-addon3"></span>
+               <input type="text" class="form-control" name="uname" value="<?php echo $_POST['uname'] ?? ''; ?>" />
+               <br> 
+               <br>Password<br><br>
+          <span class="input-group-addon" id="basic-addon3"></span>
+
+               <input type="password" class="form-control" id="password" name="password"  "/>
+               <br>
+               </h3>
+      <input type="Submit" value="Login" class="btn btn-default btn-sm">
+      
+            </div>
+        </div>
+    </div>
+  </header>
+</body>
+
+</html>
